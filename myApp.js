4af7903a88c9719b3e22c0e61dcc62cc5659fdf2
test-user <157971136+apilogtest@users.noreply.github.com>
2026-05-21 09:42:42 -0300

    fix(Exerc. 1): remove deprecated mongoose connection options

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');// Exerc. 1
 // Exerc. 1
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI);//, { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 let Person;
 
