a17909e32bddcbcbaf89b121cc809cca77505755
test-user <157971136+apilogtest@users.noreply.github.com>
2026-05-22 23:06:04 -0300

    fix 3

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');// Exerc. 1
 // Exerc. 1
-mongoose.connect(process.env.MONGO_URI); //, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.MONGO_URI); 
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
 
 
 let Person;
